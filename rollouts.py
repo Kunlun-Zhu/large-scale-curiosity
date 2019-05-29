6c9dde98a03af53040d7eb56b5d74990094fdc00
Kunlun Zhu <516455665@qq.com>
2019-05-29 10:30:04 -0400

    Update rollouts.py

--- rollouts.py
+++ rollouts.py
@@ -63,6 +63,7 @@ class Rollout(object):
                                                acs=self.buf_acs)
         '''
         self.buf_rews[:] = self.reward_fun(int_rew=0, ext_rew=self.buf_ext_rews)
+        print('current reward:', buf_rews[:])
 
     def rollout_step(self):
         t = self.step_count % self.nsteps
